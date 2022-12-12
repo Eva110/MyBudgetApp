0ac06a1bc1c63f91e2b67e827fb6bd93d6a3035d
Eva Herceg <evaherceg4@gmail.com>
2022-12-12 21:27:37 +0100

    Initial Commit

---
+++ MyBudget/MyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetApp.swift
+//  MyBudget
+//
+//  Created by User on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
